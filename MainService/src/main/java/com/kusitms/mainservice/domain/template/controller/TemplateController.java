cf8d3a2e5a7a940ce14f1501299ef41c429e7f73
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-02 15:37:47 +0900

    [feat]:Main #25 템플릿 불러오기 엔티티 생성

--- MainService/src/main/java/com/kusitms/mainservice/domain/template/controller/TemplateController.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/controller/TemplateController.java
@@ -1,24 +1,24 @@
-//package com.kusitms.mainservice.domain.template.controller;
-//
-//import com.kusitms.mainservice.domain.template.dto.TemplateResponseDto;
-//import com.kusitms.mainservice.domain.template.service.TemplateService;
-//import com.kusitms.mainservice.global.common.SuccessResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//
-//@Controller
-//@RequiredArgsConstructor
-//@RequestMapping("/api/template")
-//public class TemplateController {
-//    private final TemplateService templateService;
-//    @GetMapping
-//    public ResponseEntity<SuccessResponse<?>> getPlatform(){
-//        final TemplateResponseDto responseDto = templateService;
-//        return SuccessResponse.ok(responseDto);
-//    }
-//
-//}
+package com.kusitms.mainservice.domain.template.controller;
+
+import com.kusitms.mainservice.domain.template.dto.TemplateResponseDto;
+import com.kusitms.mainservice.domain.template.service.TemplateService;
+import com.kusitms.mainservice.global.common.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/template")
+public class TemplateController {
+    private final TemplateService templateService;
+    @GetMapping
+    public ResponseEntity<SuccessResponse<?>> getPlatformList(){
+        final TemplateResponseDto responseDto = templateService;
+        return SuccessResponse.ok(responseDto);
+    }
+
+}
--- MainService/src/main/java/com/kusitms/mainservice/domain/template/repository/TemplateRepository.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/repository/TemplateRepository.java
@@ -3,7 +3,6 @@ package com.kusitms.mainservice.domain.template.repository;
 import com.kusitms.mainservice.domain.template.domain.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// MySQL Repository
 public interface TemplateRepository extends JpaRepository<Template, Long> {
-    // MySQL 연산을 추가할 수 있음
+
 }
