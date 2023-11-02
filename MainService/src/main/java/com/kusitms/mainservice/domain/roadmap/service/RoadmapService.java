baba4dc84ebb0de185565ccd6aa7bf98c7391cca
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-03 02:44:15 +0900

    [feat]:Main #31 team별 roadmap response api service 구현

--- MainService/src/main/java/com/kusitms/mainservice/domain/roadmap/service/RoadmapService.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/roadmap/service/RoadmapService.java
@@ -2,12 +2,15 @@ package com.kusitms.mainservice.domain.roadmap.service;
 
 import com.kusitms.mainservice.domain.roadmap.domain.Roadmap;
 import com.kusitms.mainservice.domain.roadmap.domain.RoadmapDetail;
-import com.kusitms.mainservice.domain.roadmap.domain.UserRoadmap;
+import com.kusitms.mainservice.domain.roadmap.domain.RoadmapDownload;
+import com.kusitms.mainservice.domain.roadmap.domain.RoadmapTemplate;
 import com.kusitms.mainservice.domain.roadmap.dto.response.BaseRoadmapResponseDto;
 import com.kusitms.mainservice.domain.roadmap.dto.response.RoadmapDetailResponseDto;
-import com.kusitms.mainservice.domain.roadmap.dto.response.UserRoadmapResponseDto;
+import com.kusitms.mainservice.domain.roadmap.repository.RoadmapDownloadRepository;
 import com.kusitms.mainservice.domain.roadmap.repository.RoadmapRepository;
-import com.kusitms.mainservice.domain.roadmap.repository.UserRoadmapRepository;
+import com.kusitms.mainservice.domain.template.domain.TemplateDownload;
+import com.kusitms.mainservice.domain.template.dto.response.TemplateTitleResponseDto;
+import com.kusitms.mainservice.global.error.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,42 +18,43 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.kusitms.mainservice.global.error.ErrorCode.ROADMAP_DOWNLOAD_NOT_FOUND;
+
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class RoadmapService {
-    private final UserRoadmapRepository userRoadmapRepository;
+    private final RoadmapDownloadRepository roadmapDownloadRepository;
     private final RoadmapRepository roadmapRepository;
 
-    public UserRoadmapResponseDto getUserRoadmapList(Long userId) {
-        List<UserRoadmap> userRoadmapList = getUserRoadmapListFromUserId(userId);
-        List<BaseRoadmapResponseDto> baseRoadmapResponseDtoList = createBaseRoadmapResponseDtoList(userRoadmapList);
-        return UserRoadmapResponseDto.of(baseRoadmapResponseDtoList);
+    public BaseRoadmapResponseDto getTeamRoadmap(Long teamId) {
+        RoadmapDownload downloadRoadmap = getDownloadRoadmapFromTeamId(teamId);
+        Roadmap roadmap = downloadRoadmap.getRoadmap();
+        List<RoadmapDetailResponseDto> roadmapDetailResponseDtoList = createRoadmapDetailResponseDtoList(roadmap);
+        return BaseRoadmapResponseDto.of(roadmap, roadmapDetailResponseDtoList);
     }
 
-    private List<BaseRoadmapResponseDto> createBaseRoadmapResponseDtoList(List<UserRoadmap> userRoadmapList) {
-        List<Roadmap> roadmapList = getRoadmapListFromUserRoadmapList(userRoadmapList);
-        return roadmapList.stream()
-                .map(roadmap -> BaseRoadmapResponseDto.of(roadmap, createRoadmapDetailRespoonseDtoList(roadmap)))
-                .collect(Collectors.toList());
-    }
-
-    private List<Roadmap> getRoadmapListFromUserRoadmapList(List<UserRoadmap> userRoadmapList) {
-        return userRoadmapList.stream()
-                .map(UserRoadmap::getRoadmap)
-                .collect(Collectors.toList());
-    }
-
-    private List<RoadmapDetailResponseDto> createRoadmapDetailRespoonseDtoList(Roadmap roadmap) {
+    private List<RoadmapDetailResponseDto> createRoadmapDetailResponseDtoList(Roadmap roadmap) {
         List<RoadmapDetail> roadmapDetailList = roadmap.getRoadmapDetailList();
         return roadmapDetailList.stream()
-                .map(RoadmapDetailResponseDto::of)
+                .map(roadmapDetail ->
+                        RoadmapDetailResponseDto.of(
+                                roadmapDetail,
+                                createTemplateTitleResponseDtoList(roadmapDetail)))
                 .collect(Collectors.toList());
     }
 
-    private List<UserRoadmap> getUserRoadmapListFromUserId(Long userId) {
-        return userRoadmapRepository.findAllByUserId(userId);
+    private List<TemplateTitleResponseDto> createTemplateTitleResponseDtoList(RoadmapDetail roadmapDetail){
+        List<RoadmapTemplate> roadmapTemplateList = roadmapDetail.getRoadmapTemplateList();
+        return roadmapTemplateList.stream()
+                .map(roadmapTemplate -> TemplateTitleResponseDto.of(roadmapTemplate.getTemplateDownload().getTemplate()))
+                .collect(Collectors.toList());
+    }
+
+    private RoadmapDownload getDownloadRoadmapFromTeamId(Long teamId) {
+        return roadmapDownloadRepository.findByTeamId(teamId)
+                .orElseThrow(() -> new EntityNotFoundException(ROADMAP_DOWNLOAD_NOT_FOUND));
     }
 
 
