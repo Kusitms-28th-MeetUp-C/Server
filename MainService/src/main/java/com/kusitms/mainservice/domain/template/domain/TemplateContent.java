05a403817b22cfbb7111f6b2ffefb51a18e02a45
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-03 02:47:46 +0900

    [feat]:Main #31 template content data mongo db 연동

--- MainService/src/main/java/com/kusitms/mainservice/domain/template/domain/TemplateContent.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/domain/TemplateContent.java
@@ -1,7 +1,9 @@
 package com.kusitms.mainservice.domain.template.domain;
 
+import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
 @Document(collection = "template_contents")
 public class TemplateContent {
     private Long template_id;
