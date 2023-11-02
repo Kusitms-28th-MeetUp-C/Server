8451ba5420cba0dc584e6203c43de4a289a8855a
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-02 14:54:11 +0900

    [fix]:Main #28 template repository 이름 변경

--- MainService/src/main/java/com/kusitms/mainservice/domain/template/repository/TemplateRepository.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/repository/TemplateRepository.java
@@ -1,9 +1,9 @@
 package com.kusitms.mainservice.domain.template.repository;
 
-import com.kusitms.mainservice.domain.template.domain.MySQLTemplate;
+import com.kusitms.mainservice.domain.template.domain.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // MySQL Repository
-public interface MySQLRepository extends JpaRepository<MySQLTemplate, Long> {
+public interface TemplateRepository extends JpaRepository<Template, Long> {
     // MySQL 연산을 추가할 수 있음
 }
