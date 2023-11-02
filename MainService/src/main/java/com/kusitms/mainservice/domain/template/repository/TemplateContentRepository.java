669de0157cf8f6550c9cb30d56de54e20343c035
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-02 14:54:22 +0900

    [fix]:Main #28 templateContent repository 이름 변경

---
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/repository/TemplateContentRepository.java
@@ -0,0 +1,9 @@
+package com.kusitms.mainservice.domain.template.repository;
+
+
+import com.kusitms.mainservice.domain.template.domain.TemplateContent;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TemplateContentRepository extends MongoRepository<TemplateContent, String> {
+}
+
