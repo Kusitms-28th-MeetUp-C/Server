f5967a8a3ce730aedfe81fd317e0c76f63d9bd02
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-02 13:58:57 +0900

    [feat]:Main #28 Roadmap 기본 정보 전달 dto 구현

---
+++ MainService/src/main/java/com/kusitms/mainservice/domain/roadmap/dto/response/BaseRoadmapResponseDto.java
@@ -0,0 +1,25 @@
+package com.kusitms.mainservice.domain.roadmap.dto.response;
+
+import com.kusitms.mainservice.domain.roadmap.domain.Roadmap;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@Getter
+public class BaseRoadmapResponseDto {
+    private Long roadmapId;
+    private String title;
+    private String goal;
+    private List<RoadmapDetailResponseDto> roadmapDetailList;
+
+    public static BaseRoadmapResponseDto of(Roadmap roadmap, List<RoadmapDetailResponseDto> roadmapDetailList){
+        return BaseRoadmapResponseDto.builder()
+                .roadmapId(roadmap.getId())
+                .title(roadmap.getTitle())
+                .goal(roadmap.getGoal())
+                .roadmapDetailList(roadmapDetailList)
+                .build();
+    }
+}
