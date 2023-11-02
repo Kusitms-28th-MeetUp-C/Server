33988c4e7f49b780f3fa8dd92d3af46141523ee3
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-03 02:49:18 +0900

    [fix]:Main #31 template service 임시 비활성화

--- MainService/src/main/java/com/kusitms/mainservice/domain/template/service/TemplateService.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/service/TemplateService.java
@@ -1,26 +1,15 @@
-//package com.kusitms.mainservice.domain.template.service;
-//
-//import com.kusitms.mainservice.domain.template.domain.TemplateContent;
-//import com.kusitms.mainservice.domain.template.domain.MySQLTemplate;
-//import com.kusitms.mainservice.domain.template.repository.MakerRepository;
-//import com.kusitms.mainservice.domain.template.repository.MongoDBRepository;
-//import com.kusitms.mainservice.domain.template.repository.MongoRepository;
-//import com.kusitms.mainservice.domain.template.repository.MySQLRepository;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Slf4j
-//@RequiredArgsConstructor
-//@Transactional
-//@Service
-//public class TemplateService {
-//    private final MySQLRepository mySQLRepository;
-//    private final MongoDBRepository mongoDBRepository;
+package com.kusitms.mainservice.domain.template.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class TemplateService {
 //    public List<> getAllData() {
 //        List<MySQLTemplate> mySQLDataList = mySQLRepository.findAll();
 //        List<> dataCombinations = new ArrayList<>();
@@ -34,4 +23,5 @@
 //
 //        return ;
 //    }
-//}
+
+}
