03ef0c3ee5b3d26d61e4d0dfa7e2dab3ca44988b
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-03 02:45:32 +0900

    [fix]:Main #31 roadmap download entity 구현

---
+++ MainService/src/main/java/com/kusitms/mainservice/domain/roadmap/domain/RoadmapDownload.java
@@ -0,0 +1,27 @@
+package com.kusitms.mainservice.domain.roadmap.domain;
+
+import com.kusitms.mainservice.domain.user.domain.Team;
+import com.kusitms.mainservice.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "roadmap_download")
+@Entity
+public class RoadmapDownload {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "roadmap_download_id")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "roadmap_id")
+    private Roadmap roadmap;
+    @OneToOne
+    private Team team;
+}
