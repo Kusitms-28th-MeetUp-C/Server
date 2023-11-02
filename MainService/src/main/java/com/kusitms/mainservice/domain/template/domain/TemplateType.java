eb596ab1514defc29f67697d082645da76ddeec4
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-03 02:46:44 +0900

    [feat]:Main #31 template type converter 추가

--- MainService/src/main/java/com/kusitms/mainservice/domain/template/domain/TemplateType.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/domain/TemplateType.java
@@ -1,7 +1,30 @@
 package com.kusitms.mainservice.domain.template.domain;
 
+import com.kusitms.mainservice.global.error.exception.InvalidValueException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+import static com.kusitms.mainservice.global.error.ErrorCode.INVALID_TEMPLATE_TYPE;
+
+@RequiredArgsConstructor
+@Getter
 public enum TemplateType {
-    TYPE1,
-    TYPE2,
-    TYPE3
+    IT("it"),
+    SURVEY_DATA_ANALYSIS("survey_data_analysis"),
+    CORPORATE_ANALYSIS("corporate_analysis"),
+    PT("pt"),
+    DESIGN("design"),
+    MARKETING("marketing"),
+    ETC("etc");
+
+    private final String stringTemplateType;
+
+    public static TemplateType getEnumPeriodFromStringPeriod(String stringTemplateType) {
+        return Arrays.stream(values())
+                .filter(period -> period.stringTemplateType.equals(stringTemplateType))
+                .findFirst()
+                .orElseThrow(() -> new InvalidValueException(INVALID_TEMPLATE_TYPE));
+    }
 }
