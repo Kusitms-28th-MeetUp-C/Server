3e5b5089d693b9c56e0f6bfee2ac0ef5afba1b64
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-03 23:45:58 +0900

    [feat]:Main #33 Team & space mapping 관계 정의

--- MainService/src/main/java/com/kusitms/mainservice/domain/user/domain/Team.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/user/domain/Team.java
@@ -5,6 +5,9 @@ import com.kusitms.mainservice.domain.roadmap.domain.RoadmapDownload;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
@@ -16,12 +19,31 @@ public class Team {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "team_id")
     private Long id;
+    private String title;
     @Enumerated(EnumType.STRING)
-    private TeamSpace teamSpace;
+    private TeamType teamType;
     private String introduction;
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
     @OneToOne
     private RoadmapDownload roadmapDownload;
+    @OneToMany(mappedBy = "team")
+    @Builder.Default
+    private List<TeamSpace> teamSpaceList = new ArrayList<>();
+
+    public static Team createTeam(String title, TeamType teamType, String introduction, User user) {
+        Team team = Team.builder()
+                .title(title)
+                .teamType(teamType)
+                .introduction(introduction)
+                .user(user)
+                .build();
+        user.updateTeamList(team);
+        return team;
+    }
+
+    public void addTeamSpaceList(TeamSpace teamSpace){
+        this.teamSpaceList.add(teamSpace);
+    }
 }
--- MainService/src/main/java/com/kusitms/mainservice/domain/user/domain/TeamSpace.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/user/domain/TeamSpace.java
@@ -1,4 +1,32 @@
 package com.kusitms.mainservice.domain.user.domain;
 
-public enum TeamSpace {
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "team_space")
+@Entity
+public class TeamSpace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_space_id")
+    private Long id;
+    private String url;
+    @Enumerated(EnumType.STRING)
+    private TeamSpaceType teamSpaceType;
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public static TeamSpace createTeamSpace(String url, TeamSpaceType teamSpaceType, Team team){
+        TeamSpace teamSpace = TeamSpace.builder()
+                .url(url)
+                .teamSpaceType(teamSpaceType)
+                .build();
+        team.addTeamSpaceList(teamSpace);
+        return teamSpace;
+    }
 }
