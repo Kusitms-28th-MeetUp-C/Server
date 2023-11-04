2be49f7843139112cd7ba10f592cd696b9e33542
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-05 02:18:19 +0900

    [refactor]:Main #35 CustomRoadmap & CustomTemplate domain 이동

--- MainService/src/main/java/com/kusitms/mainservice/domain/user/controller/UserController.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.kusitms.mainservice.domain.user.controller;
 
 
+import com.kusitms.mainservice.domain.team.dto.request.TeamRequestDto;
+import com.kusitms.mainservice.domain.team.dto.request.TeamRoadmapRequestDto;
+import com.kusitms.mainservice.domain.team.dto.request.UpdateTeamRequestDto;
 import com.kusitms.mainservice.domain.user.dto.request.*;
-import com.kusitms.mainservice.domain.user.dto.response.TeamResponseDto;
+import com.kusitms.mainservice.domain.team.dto.response.TeamResponseDto;
 import com.kusitms.mainservice.domain.user.dto.response.UserAuthResponseDto;
 import com.kusitms.mainservice.domain.user.service.AuthService;
-import com.kusitms.mainservice.domain.user.service.TeamService;
+import com.kusitms.mainservice.domain.team.service.TeamService;
 import com.kusitms.mainservice.global.common.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
     private final AuthService authService;
-    private final TeamService teamService;
 
     @PostMapping("/signIn")
     public ResponseEntity<SuccessResponse<?>> signIn(@RequestParam final String authToken,
@@ -30,25 +32,5 @@ public class UserController {
                                                      @RequestBody final UserSignUpRequestDto requestDto) {
         final UserAuthResponseDto responseDto = authService.signUp(requestDto, authToken);
         return SuccessResponse.ok(responseDto);
-    }
-
-    @PostMapping("/team")
-    public ResponseEntity<SuccessResponse<?>> createTeam(@RequestHeader final Long userId,
-                                                         @RequestBody final TeamRequestDto requestDto) {
-        final TeamResponseDto responseDto = teamService.createTeam(userId, requestDto);
-        return SuccessResponse.ok(responseDto);
-    }
-
-    @PatchMapping("/team")
-    public ResponseEntity<SuccessResponse<?>> updateTeamInfo(@RequestBody final UpdateTeamRequestDto requestDto) {
-        final TeamResponseDto responseDto = teamService.updateTeamInfo(requestDto);
-        return SuccessResponse.ok(responseDto);
-    }
-
-    @PostMapping("/team/roadmap")
-    public ResponseEntity<SuccessResponse<?>> addTeamRoadmap(@RequestHeader final Long userId,
-                                                             @RequestBody final TeamRoadmapRequestDto requestDto) {
-        teamService.addTeamRoadmap(userId, requestDto);
-        return SuccessResponse.ok(null);
     }
 }
