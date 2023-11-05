a760dee678f9e7765a898a77f9d594a12b1f3953
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-06 02:50:25 +0900

    [feat]:Main #39 userId를 이용해 team을 조회하는 repository method 구현

--- MainService/src/main/java/com/kusitms/mainservice/domain/team/repository/TeamRepository.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/team/repository/TeamRepository.java
@@ -3,6 +3,9 @@ package com.kusitms.mainservice.domain.team.repository;
 import com.kusitms.mainservice.domain.team.domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
     boolean existsTeamByTitle(String title);
+    List<Team> findAllByUserId(Long userId);
 }
