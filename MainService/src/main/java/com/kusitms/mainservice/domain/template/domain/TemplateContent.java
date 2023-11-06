c515c0b49fc2c9ec5b427aa65f376668f58ae4eb
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-07 01:10:10 +0900

    [refactor]:Main #25 템플릿 상세보기 테스트 코드 정리

--- MainService/src/main/java/com/kusitms/mainservice/domain/template/domain/TemplateContent.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/domain/TemplateContent.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Getter
 @Document(collection = "template_contents")
 public class TemplateContent {
-    private Long template_id;
+    private Long templateId;
     private String content;
     private String introduction;
 }
