fbf2df637bd27cf1d98efe72e24bae5f9d89c094
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-07 18:28:35 +0900

    [feat]:Socket #42 검색용 template domain 구현

---
+++ SocketService/src/main/java/com/kusitms/socketservice/domain/search/domain/SearchCustomTemplate.java
@@ -0,0 +1,18 @@
+package com.kusitms.socketservice.domain.search.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Builder
+@Getter
+@Document(collection = "search_custom_template")
+public class SearchCustomTemplate {
+    private Long userId;
+    private Long templateId;
+    private String title;
+    private String relatedTeamTitle;
+    private String relatedRoadmapTitle;
+    private TemplateType templateType;
+    private boolean isOpend;
+}
