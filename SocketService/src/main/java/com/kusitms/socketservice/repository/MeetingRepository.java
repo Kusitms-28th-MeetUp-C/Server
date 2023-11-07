acb52b3033eaae69ac68abcfb85304c317b9250e
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-07 18:07:35 +0900

    [del]:Socket #42 불필요 파일 제거

--- SocketService/src/main/java/com/kusitms/socketservice/repository/MeetingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kusitms.socketservice.repository;
-
-import com.kusitms.socketservice.domain.MeetingRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MeetingRepository extends JpaRepository<MeetingRoom, Long> {
-}
