acc13ba7fb9b00e5d3cd888a83f1f96f638a5731
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-08 02:34:27 +0900

    [feat]:Main #44 나의 회의록 template 상세 조회 api controller 구현

--- MainService/src/main/java/com/kusitms/mainservice/domain/template/controller/CustomTemplateController.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/controller/CustomTemplateController.java
@@ -1,13 +1,35 @@
-//package com.kusitms.mainservice.domain.template.controller;
-//
-//import com.kusitms.mainservice.domain.template.service.CustomTemplateService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RequiredArgsConstructor
-//@RequestMapping("/api/template/roadmap")
-//@RestController
-//public class CustomTemplateController {
-//    private final CustomTemplateService customTemplateService;
-//}
+package com.kusitms.mainservice.domain.template.controller;
+
+import com.kusitms.mainservice.domain.template.dto.response.CustomTemplateDetailResponseDto;
+import com.kusitms.mainservice.domain.template.dto.response.TemplateDownloadDetailResponseDto;
+import com.kusitms.mainservice.domain.template.service.CustomTemplateService;
+import com.kusitms.mainservice.global.common.SuccessResponse;
+import com.kusitms.mainservice.global.config.auth.UserId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/custom/template")
+@RestController
+public class CustomTemplateController {
+    private final CustomTemplateService customTemplateService;
+
+    @GetMapping("/detail/team")
+    public ResponseEntity<SuccessResponse<?>> getCustomTemplateDetailInfo(@UserId final Long userId,
+                                                                          @RequestParam final Long templateId,
+                                                                          @RequestParam final String roadmapTitle,
+                                                                          @RequestParam final String teamTitle) {
+        final CustomTemplateDetailResponseDto responseDto = customTemplateService.getCustomTemplateDetailInfo(userId, roadmapTitle, teamTitle, templateId);
+        return SuccessResponse.ok(responseDto);
+    }
+
+    @GetMapping("/detail")
+    public ResponseEntity<SuccessResponse<?>> getTemplateDetailInfo(@RequestParam final Long templateId) {
+        final TemplateDownloadDetailResponseDto responseDto = customTemplateService.getTemplateDetailInfo(templateId);
+        return SuccessResponse.ok(responseDto);
+    }
+}
