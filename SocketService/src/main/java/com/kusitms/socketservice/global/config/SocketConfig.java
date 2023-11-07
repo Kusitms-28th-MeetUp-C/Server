c46ae3aa51a1e1113607f31beec60220814e229b
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-07 18:24:54 +0900

    [feat]:Socket #42 message protocol resolver 구현

--- SocketService/src/main/java/com/kusitms/socketservice/global/config/SocketConfig.java
+++ SocketService/src/main/java/com/kusitms/socketservice/global/config/SocketConfig.java
@@ -1,16 +1,20 @@
 package com.kusitms.socketservice.global.config;
 
 import com.kusitms.socketservice.global.common.AuthenticationInterceptor;
+import com.kusitms.socketservice.global.config.auth.SessionIdArgumentResolver;
 import com.kusitms.socketservice.global.error.handler.MessageErrorHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Order(Ordered.HIGHEST_PRECEDENCE + 50)
@@ -19,6 +23,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class SocketConfig implements WebSocketMessageBrokerConfigurer {
     private final AuthenticationInterceptor authenticationInterceptor;
     private final MessageErrorHandler messageErrorHandler;
+    private final SessionIdArgumentResolver sessionIdArgumentResolver;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -36,5 +41,10 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(authenticationInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(sessionIdArgumentResolver);
     }
 }
