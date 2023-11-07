8b54eeeade5ae4f21bae903c0c0ef5b02263cbf5
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-08 02:36:01 +0900

    [feat]:Main #44 userId와 title로 team 조회 메서드 구현

--- MainService/src/main/java/com/kusitms/mainservice/domain/team/repository/TeamRepository.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/team/repository/TeamRepository.java
@@ -7,8 +7,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    Optional<Team> findByUserIdAndTitle(Long userId, String title);
+
     boolean existsTeamByTitle(String title);
 
     List<Team> findAllByUserId(Long userId);
+
     Optional<Team> findById(Long Id);
 }
