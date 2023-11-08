e6bb486a182a068f4a75474c5be97c238d984647
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-08 17:50:39 +0900

    [feat]:Main #46 template or roadmap maker response dto 구현

---
+++ MainService/src/main/java/com/kusitms/mainservice/domain/user/dto/response/MakerResponseDto.java
@@ -0,0 +1,22 @@
+package com.kusitms.mainservice.domain.user.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class MakerResponseDto {
+    String name;
+    int templateCount;
+    int roadmapCount;
+    Long sessionId;
+
+    public static MakerResponseDto of(String name, int templateCount, int roadmapCount, Long sessionId){
+        return MakerResponseDto.builder()
+                .name(name)
+                .templateCount(templateCount)
+                .roadmapCount(roadmapCount)
+                .sessionId(sessionId)
+                .build();
+    }
+}
