ae753523571274970cd85f6c8fdd66367a2525df
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-13 03:52:24 +0900

    [feat]:Main #63 article dto생성

--- MainService/src/main/java/com/kusitms/mainservice/domain/article/dto/response/ArticleResponseDto.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/article/dto/response/ArticleResponseDto.java
@@ -1,5 +1,20 @@
 package com.kusitms.mainservice.domain.article.dto.response;
 
+import com.kusitms.mainservice.domain.article.domain.Article;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class ArticleResponseDto {
-    private
+    private Long id;
+    private String title;
+    private String image;
+    public static ArticleResponseDto of(Article article){
+        return ArticleResponseDto.builder()
+                .id(article.getId())
+                .title(article.getTitle())
+                .image(article.getImage())
+                .build();
+    }
 }
