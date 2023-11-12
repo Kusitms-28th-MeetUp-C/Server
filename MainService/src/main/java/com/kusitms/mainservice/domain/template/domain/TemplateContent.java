22c083b32a99fa743751fe4813d7b28638162826
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-13 02:27:28 +0900

    [fix]:Main #57 template content field 명시

--- MainService/src/main/java/com/kusitms/mainservice/domain/template/domain/TemplateContent.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/domain/TemplateContent.java
@@ -3,7 +3,9 @@ package com.kusitms.mainservice.domain.template.domain;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @Setter
@@ -11,6 +13,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class TemplateContent {
     @Id
     private String id;
+    @Indexed(unique = true)
+    @Field(name = "template_id")
     private Long templateId;
     private Long agendaNum;
     private String agenda;
