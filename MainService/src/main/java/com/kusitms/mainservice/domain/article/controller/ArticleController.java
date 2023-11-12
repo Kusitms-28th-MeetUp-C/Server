951619deecbf4f85ecfa9c477338222b0dd4a6bf
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-13 04:02:45 +0900

    [feat]:Main #63 article 미리보기 controller 구현

--- MainService/src/main/java/com/kusitms/mainservice/domain/article/controller/ArticleController.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/article/controller/ArticleController.java
@@ -1,4 +1,26 @@
 package com.kusitms.mainservice.domain.article.controller;
 
+import com.kusitms.mainservice.domain.article.dto.response.ArticleResponseDto;
+import com.kusitms.mainservice.domain.article.service.ArticleService;
+import com.kusitms.mainservice.global.common.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/article")
+@RestController
 public class ArticleController {
+    private final ArticleService articleService;
+
+    @GetMapping("/get")
+    public ResponseEntity<SuccessResponse<?>> getAritcleList(){
+        final List<ArticleResponseDto> articleResponseDtoList = articleService.getArticleList();
+        return SuccessResponse.ok(articleResponseDtoList);
+    }
+
 }
