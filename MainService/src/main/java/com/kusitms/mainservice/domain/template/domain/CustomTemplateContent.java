81c112f61f8da0382ca18e5d493625b3b1bca62f
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-13 02:24:38 +0900

    [feat]:Main #57 custom template content 객체 생성

---
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/domain/CustomTemplateContent.java
@@ -0,0 +1,24 @@
+package com.kusitms.mainservice.domain.template.domain;
+
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Getter
+@Builder
+@Document(collection = "custom_template_contents")
+public class CustomTemplateContent {
+    @Id
+    @Field(name = "_id")
+    private String id;
+
+    @Field(name = "template_id")
+    @Indexed(unique = true)
+    private Long templateId;
+
+    @Field(name = "content")
+    private String content;
+}
