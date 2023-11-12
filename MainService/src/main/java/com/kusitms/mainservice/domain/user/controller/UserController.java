6f3bbba0778a9224fc54e19dd2c360ca4b94165e
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-12 21:03:52 +0900

    [fix]:Main #57 Authorization header 추가

--- MainService/src/main/java/com/kusitms/mainservice/domain/user/controller/UserController.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/user/controller/UserController.java
@@ -1,23 +1,14 @@
 package com.kusitms.mainservice.domain.user.controller;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kusitms.mainservice.domain.user.domain.OAuthToken;
-import com.kusitms.mainservice.domain.user.dto.request.*;
-import com.kusitms.mainservice.domain.team.dto.response.TeamResponseDto;
+import com.kusitms.mainservice.domain.user.dto.request.UserSignInRequestDto;
+import com.kusitms.mainservice.domain.user.dto.request.UserSignUpRequestDto;
 import com.kusitms.mainservice.domain.user.dto.response.UserAuthResponseDto;
 import com.kusitms.mainservice.domain.user.service.AuthService;
-import com.kusitms.mainservice.domain.team.service.TeamService;
 import com.kusitms.mainservice.global.common.SuccessResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
@@ -26,14 +17,14 @@ public class UserController {
     private final AuthService authService;
 
     @PostMapping("/signIn")
-    public ResponseEntity<SuccessResponse<?>> signIn(@RequestParam final String authToken,
+    public ResponseEntity<SuccessResponse<?>> signIn(@RequestHeader("Authorization") final String authToken,
                                                      @RequestBody final UserSignInRequestDto requestDto) {
         final UserAuthResponseDto responseDto = authService.signIn(requestDto, authToken);
         return SuccessResponse.ok(responseDto);
     }
 
     @PostMapping("/signUp")
-    public ResponseEntity<SuccessResponse<?>> signUp(@RequestParam final String authToken,
+    public ResponseEntity<SuccessResponse<?>> signUp(@RequestHeader("Authorization") final String authToken,
                                                      @RequestBody final UserSignUpRequestDto requestDto) {
         final UserAuthResponseDto responseDto = authService.signUp(requestDto, authToken);
         return SuccessResponse.ok(responseDto);
