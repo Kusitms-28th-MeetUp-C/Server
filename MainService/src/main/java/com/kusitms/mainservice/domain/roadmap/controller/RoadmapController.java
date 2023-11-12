f068fc83ca364278a668e8b2cd4af9479ee6ff53
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-13 01:39:14 +0900

    [refactor]:Main #54 로드맵 탐색하기 페이지 사이즈 지정

--- MainService/src/main/java/com/kusitms/mainservice/domain/roadmap/controller/RoadmapController.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/roadmap/controller/RoadmapController.java
@@ -11,6 +11,7 @@ import com.kusitms.mainservice.domain.template.dto.response.TemplateDetailRespon
 import com.kusitms.mainservice.global.common.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +22,7 @@ public class RoadmapController {
     private final RoadmapService roadmapService;
 
     @PostMapping("/get")
-    public ResponseEntity<SuccessResponse<?>> getRoadmapByTitleAndRoadmapType(@RequestBody SearchRoadmapRequestDto searchRoadmapRequestDto, Pageable pageable){
+    public ResponseEntity<SuccessResponse<?>> getRoadmapByTitleAndRoadmapType(@RequestBody SearchRoadmapRequestDto searchRoadmapRequestDto, @PageableDefault(size = 12) Pageable pageable){
         final SearchRoadmapResponseDto searchRoadmapResponseDto = roadmapService.searchRoadmapByTitleAndRoadmapType(searchRoadmapRequestDto, pageable);
         return SuccessResponse.ok(searchRoadmapResponseDto);
     }
