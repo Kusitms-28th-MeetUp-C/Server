7a5aa2313e19f3139284ef76b0554e9c9a2ec78a
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-15 00:07:15 +0900

    [feat]:Main #83 roadmap 완료 버튼 api 구현

--- MainService/src/main/java/com/kusitms/mainservice/domain/roadmap/controller/CustomRoadmapController.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/roadmap/controller/CustomRoadmapController.java
@@ -1,12 +1,26 @@
 package com.kusitms.mainservice.domain.roadmap.controller;
 
+import com.kusitms.mainservice.domain.roadmap.dto.request.SearchRoadmapRequestDto;
+import com.kusitms.mainservice.domain.roadmap.dto.response.SearchBaseRoadmapResponseDto;
+import com.kusitms.mainservice.domain.roadmap.service.CustomRoadmapService;
+import com.kusitms.mainservice.global.common.SuccessResponse;
+import com.kusitms.mainservice.global.config.auth.UserId;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/custom/roadmap")
 @RestController
 public class CustomRoadmapController {
-
+    private final CustomRoadmapService customRoadmapService;
+    @PatchMapping("/space")
+    public ResponseEntity<SuccessResponse<?>> updateCompletedRoadmapSpace(@RequestParam final Long stepId,
+                                                                          @RequestParam final boolean isCompleted){
+        customRoadmapService.updateCompletedRoadmapSpace(stepId, isCompleted);
+        return SuccessResponse.ok(null);
+    }
 }
