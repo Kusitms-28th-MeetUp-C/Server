cbbc5d54657d1a96e2e13b6dfaf824f0a5a32758
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-15 23:22:40 +0900

    [feat]:Socket #90 나의 회의록 조회 response 수정

--- SocketService/src/main/java/com/kusitms/socketservice/domain/search/dto/response/SearchResultElementResponseDto.java
+++ SocketService/src/main/java/com/kusitms/socketservice/domain/search/dto/response/SearchResultElementResponseDto.java
@@ -1,26 +1,37 @@
 package com.kusitms.socketservice.domain.search.dto.response;
 
-import com.kusitms.socketservice.domain.search.domain.SearchResultType;
+import com.kusitms.socketservice.domain.search.domain.SearchUserTemplate;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 @Getter
 public class SearchResultElementResponseDto {
-    private Long id;
-    private SearchResultType resultType;
+    private Long templateId;
     private String title;
     private String teamTitle;
     private boolean isOpen;
     private String category;
 
-    public static SearchResultElementResponseDto of(Long id, SearchResultType resultType, String title,
+    public static List<SearchResultElementResponseDto> listOf(List<SearchUserTemplate> searchUserTemplateList){
+        return searchUserTemplateList.stream()
+                .map(searchUserTemplate ->
+                        SearchResultElementResponseDto.of(
+                                searchUserTemplate.getTemplateId(),
+                                searchUserTemplate.getTitle(),
+                                searchUserTemplate.getRelatedTeamTitle(),
+                                searchUserTemplate.isOpened(),
+                                searchUserTemplate.getTemplateType().toString()
+                        )).collect(Collectors.toList());
+    }
+
+    public static SearchResultElementResponseDto of(Long templateId, String title,
                                                     String teamTitle, boolean isOpen, String category){
         return SearchResultElementResponseDto.builder()
-                .id(id)
-                .resultType(resultType)
+                .templateId(templateId)
                 .title(title)
                 .teamTitle(teamTitle)
                 .isOpen(isOpen)
