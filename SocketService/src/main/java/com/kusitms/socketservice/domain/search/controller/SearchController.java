2c644d03eef785d8009b1fb239a2b04880cec745
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-16 00:23:30 +0900

    [feat]:Socket #106 response 형식 적용

--- SocketService/src/main/java/com/kusitms/socketservice/domain/search/controller/SearchController.java
+++ SocketService/src/main/java/com/kusitms/socketservice/domain/search/controller/SearchController.java
@@ -3,6 +3,8 @@ package com.kusitms.socketservice.domain.search.controller;
 import com.kusitms.socketservice.domain.search.dto.request.SearchRequestDto;
 import com.kusitms.socketservice.domain.search.dto.response.SearchResultResponseDto;
 import com.kusitms.socketservice.domain.search.service.SearchService;
+import com.kusitms.socketservice.global.common.MessageSuccessCode;
+import com.kusitms.socketservice.global.common.MessageSuccessResponse;
 import com.kusitms.socketservice.global.config.auth.SessionId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -19,7 +21,7 @@ public class SearchController {
     public void getSearchResult(@SessionId final Long sessionId,
                                 final SearchRequestDto searchRequestDto) {
         final SearchResultResponseDto responseDto = searchService.getSearchResult(sessionId, searchRequestDto);
-        template.convertAndSend("/sub/search/" + sessionId, responseDto);
+        template.convertAndSend("/sub/search/" + sessionId, MessageSuccessResponse.of(MessageSuccessCode.SEARCH, responseDto));
     }
 
 //    @MessageExceptionHandler
