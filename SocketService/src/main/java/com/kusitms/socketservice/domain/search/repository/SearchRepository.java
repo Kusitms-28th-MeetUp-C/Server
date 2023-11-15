9bebcbb26e6ac23272f306a81a364a1f40ca16c5
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-15 23:20:37 +0900

    [feat]:Socket #90 Search 추상화 Repository 구현

---
+++ SocketService/src/main/java/com/kusitms/socketservice/domain/search/repository/SearchRepository.java
@@ -0,0 +1,9 @@
+package com.kusitms.socketservice.domain.search.repository;
+
+import com.kusitms.socketservice.domain.search.domain.SearchUserTemplate;
+
+import java.util.List;
+
+public interface SearchRepository {
+    List<SearchUserTemplate> findAllBySearchText(String searchText, String userId);
+}
