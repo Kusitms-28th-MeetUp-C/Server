0addf50199c9a79bcbf94d3fa32b3945c64e91b2
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-18 19:09:10 +0900

    [feat]:Main #131 템플릿 삭제 api 구현

--- MainService/src/main/java/com/kusitms/mainservice/domain/template/controller/TemplateManageController.java
+++ MainService/src/main/java/com/kusitms/mainservice/domain/template/controller/TemplateManageController.java
@@ -33,8 +33,8 @@ public class TemplateManageController {
         return SuccessResponse.created(createTemplateResponseDto);
     }
 
-    @DeleteMapping("/{templateId}")
-    public ResponseEntity<SuccessResponse<?>> deleteTemplate(@PathVariable Long templateId) {
+    @DeleteMapping
+    public ResponseEntity<SuccessResponse<?>> deleteTemplate(@RequestParam Long templateId) {
         templateManageService.deleteTemplateByTemplateId(templateId);
         return ResponseEntity.ok(null);
     }
